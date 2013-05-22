f51cf06eb6f98ab2664a8a074a8549624713b244
Woo Jin Park <woojinp@gmail.com>
2013-05-21 22:29:15 -0700

    added separate view for completed items

---
+++ js/todo.js
@@ -0,0 +1,102 @@
+var Todos;
+
+$(document).ready(function(){
+
+  // model
+  var Todo = Backbone.Model.extend({
+    // attributes: title, createdAt, complete
+    defaults: function() {
+      return {
+        title: "No title entered",
+        createdAt: new Date(),
+        complete: false
+      };
+    },
+
+    toggle: function() {
+      this.save({complete: !this.get("complete")});
+    }
+  });
+
+  // collection
+  var TodoList = Backbone.Collection.extend({
+    model: Todo,
+
+    localStorage: new Backbone.LocalStorage('todos'),
+
+    completed: function() {
+      return this.filter(function(todo) {
+        return todo.get('complete') === true;
+      });
+    },
+
+    incomplete: function() {
+      return this.filter(function(todo) {
+        return !todo.get('complete');
+      });
+    }
+  });
+
+
+  Todos = new TodoList();
+
+  var TodoView = Backbone.View.extend({
+    tagName: 'li',
+    template: _.template($('#list-item-template').html()),
+    events: {
+      // $('.checkbox').on(click, toggleView);
+      "click .checkbox": function() {
+        this.model.toggle();
+      }
+    },
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  var AppView = Backbone.View.extend({
+    el: $('#todoapp'),
+    events: {
+      "click #button": function() {
+        // Todo.create({title => "foo"})
+        Todos.create({title: this.input.val()});
+        this.input.val('');
+      }
+    },
+    initialize: function() {
+      this.input = this.$('#new-todo');
+      this.counter = this.$('#counter');
+      this.listenTo(Todos, 'add', this.appendTodo);
+      this.listenTo(Todos, 'all', this.render);
+      Todos.fetch();
+    },
+
+    appendTodo: function(todo) {
+      var view = new TodoView({model: todo});
+      if (todo.get("complete")) {
+        this.$('#completed-list').append(view.render().el);
+      } else {
+        this.$('#todo-list').append(view.render().el);
+      }
+    },
+
+    addTodo: function() {
+      Todos.create({title: this.input.val()});
+      this.input.val('');
+    },
+
+    counterTemplate: _.template($('#counter-template').html()),
+    render: function() {
+      var completed = Todos.completed().length;
+      var incomplete = Todos.incomplete().length;
+      this.counter.html(this.counterTemplate({
+        completed: completed,
+        incomplete: incomplete
+      }));
+    }
+  });
+
+  var App = new AppView();
+
+});
